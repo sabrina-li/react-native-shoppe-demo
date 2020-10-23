bea9e7d5edbe56bdcd59650c032cc2bfe53b8f6e
Sabrina Li <sabrinali0312@gmail.com>
2020-10-23 10:35:35 -0400

    add FS RN and onReady

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,12 @@
 
 import React from 'react';
 import AppNavigator from './navigation'
+import FullStory from '@fullstory/react-native';
+import { Platform } from 'react-native';
 
 const App: () => React.ReactNode = () => {
+  FullStory.getCurrentSessionURL().then(url => console.log(`FS URL: ${Platform.OS}  ${url}`));
+
   return (
     <AppNavigator />
   );
